134a9f12c1653e0620c1633ea61e095a0036c69b
dilshadkhan04 <159414259+dilshadkhan04@users.noreply.github.com>
2025-01-29 22:48:26 +0530

    Create array_1.sv

---
+++ array_1.sv
@@ -0,0 +1,17 @@
+module array_1;  //multidimensional array
+  reg [3:0] matrix [0:2][0:2]; // 3x3 matrix
+  
+  initial begin
+    matrix[0][0] = 4'h1;
+    matrix[0][1] = 4'h2;
+    matrix[0][2] = 4'h3;
+    matrix[1][0] = 4'h4;
+    matrix[1][1] = 4'h5;
+    matrix[1][2] = 4'h6;
+    matrix[2][0] = 4'h7;
+    matrix[2][1] = 4'h8;
+    matrix[2][2] = 4'h9;
+
+    $display("Matrix[1][1]: %h", matrix[1][1]); // Should print 5
+  end
+endmodule
