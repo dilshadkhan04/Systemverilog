01aad6ed54c8935f25292a0791ef926ec7ba3505
dilshadkhan04 <159414259+dilshadkhan04@users.noreply.github.com>
2025-02-04 21:33:56 +0530

    Change

---
+++ 2.Function and Task/function_1.sv
@@ -0,0 +1,41 @@
+module tb;
+
+  int unsigned test;
+
+  int unsigned  res;
+
+  int unsigned  a = 12;
+
+  int unsigned  b = 11;
+
+  function  int unsigned mul();
+
+  return a*b;
+
+  endfunction
+
+ 
+
+initial begin
+
+  test = a*b;
+
+  res = mul();
+
+  $display("Answer is : %0d",res);
+
+  if(test == res) begin
+
+    $display("Test passed");
+
+  end
+
+else begin
+
+  $display("Test failed");
+
+end
+
+end
+
+endmodule
